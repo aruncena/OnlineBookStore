2f0c81caf710374c2ee907747bd4133704cc1ec7
Arunkumar T <74063139+aruncena@users.noreply.github.com>
2024-01-13 12:04:30 +0530

    Update IDatabase.java

--- onlinebookstore-J2EE/OnlineBookStore/constants/IDatabase.java
+++ onlinebookstore-J2EE/OnlineBookStore/constants/IDatabase.java
@@ -3,8 +3,8 @@ package constants;
 public class IDatabase {
 
 	public static final String DRIVER_NAME = "com.mysql.jdbc.Driver";
-	public static final String CONNECTION_STRING = "jdbc:mysql://localhost:3306/onlinebookstore";
+	public static final String CONNECTION_STRING = "jdbc:mysql://192.168.0.12:3306/onlinebookstore";
 	public static final String USER_NAME = "root";
-	public static final String PASSWORD = "sHj@6378#jw";
+	public static final String PASSWORD = "arun@1234";
 
 }
